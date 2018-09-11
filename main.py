754a42e7a2072ea577e085a75eac155a5787d301
Rey David <rdominguez@tecnologer.net>
2018-09-10 23:51:56 -0700

    Improvements
    + Responses with stickers
    + Allow one command /hell or /heaven every 30 seconds
    + Send to hell with sticker 'ticket to hell'

--- main.py
+++ main.py
@@ -4,50 +4,21 @@ import time
 import random
 import os
 from pprint import pprint
+import dao
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 bot = telepot.Bot('684372282:AAEHSrUFkvdOoCci8c0FzZ0H39BGfJ5Zbxc')  # token
 
-stats = {
-    'PAKOY3K': {
-        'hell': 90,
-        'heaven': 1
-    },
-    'XHAUL': {
-        'hell': 2,
-        'heaven': 0
-    },
-    'VSANCHEZ': {
-        'hell': 3,
-        'heaven': 0
-    },
-    'TECNOLOGER': {
-        'hell': 3,
-        'heaven': 2
-    }
-}
+timeout = {}
+ticketToHell={}
 
-awnsers = {
-    'hell': [
-        'eso es todo, sigue asi',
-        'una raya mas al tigre',
-        'ha ha!',
-        'anda la osa!',
-        'fierro pariente... eso te espera xD',
-        u'\U0001f608',
-        u'\U0001f449\U0001f44c',
-        'peleishon de bananeishon!'
-    ],
-    'heaven': [
-        'una de cal por las que van de arena',
-        'no importa,tu bien sabes a donde perteneces',
-        'amen!',
-        'ni tu te la crees',
-        'el pastel era mentira',
-        'santa no existe',
-        u'\u2b50\ufe0f'
-    ]
-}
+tranquiloviejo = u"CAADAQADNwADzxSlAAEpVbCJbOTMsAI"
+awanta = u'CAADAQADqwADJaHuBMhw3ty2zbpjAg'
+dejesedemamadas = u'CAADAQAD7wEAAs8UpQABdurS64LRGooC'
+alapifu = u"CAADAQADkQADJaHuBAABSnzPxbzjJQI"
+ticket = u'CAADAQADnQADJaHuBGvY1E43XYjJAg'
+terco = u'CAADAQADqgADJaHuBEK37px2YeW-Ag'
 
 def handle(msg):
     pprint(msg)
@@ -67,66 +38,131 @@ def reply(msg, response):
     msgId = msg['message_id']
     bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msgId)
 
+
+def replySticker(msg, sticker):
+    chat_id = msg['chat']['id']
+    msgId = msg['message_id']
+    bot.sendSticker(chat_id=chat_id, sticker=sticker, reply_to_message_id=msgId)
+
 def getAwnser(type):
-    i = random.randint(0, len(awnsers[type])-1)
-    return awnsers[type][i]
+    return dao.GetAnswer(type)
+
+
+def newRecord(sender):
+    timeout[sender] = {"time": time.time(), "count": 0}
+
+
+def waitToHell(msg):
+    sender = getUserSender(msg)
+    chat_id = msg['chat']['id']
+    user_id = msg["from"]['id']
+    
+    reply(msg, "a quien le damos ese?")    
+    newRecord(sender)
+
+    ticketToHell[chat_id]=user_id
+
+
+def validTimeout(msg, sender):
+    if not sender in timeout:
+        return True
+
+    elapsed = int(time.time()-timeout[sender]["time"])
+
+    if elapsed <= 30:
+        timeout[sender]["count"]+=1
+
+        if timeout[sender]["count"] == 1:
+            replySticker(msg, tranquiloviejo)
+        elif timeout[sender]["count"] == 5:
+            replySticker(msg, awanta)
+        elif timeout[sender]["count"] == 10:
+            replySticker(msg, dejesedemamadas)
+        elif timeout[sender]["count"] == 20:
+            replySticker(msg, alapifu)
+            replySticker(msg, dejesedemamadas)
+        elif timeout[sender]["count"] == 30:
+            replySticker(msg, terco)
+
+        return False
+
+    return True
 
 def on_chat_message(msg):
-
-    if isBot(msg):
+    # if not has text or sticker
+    if isBot(msg) or (not 'text' in msg and not 'sticker' in msg):
         return
-    # pprint(msg)
-    # pprint(msg['text'])
-    cmds = msg['text'].split(' ')
+
     cmd = ''
     user = ''
-    response = ''
-    if len(cmds) >= 2:
-        cmd = cmds[0]
-        user = cmds[1].upper().replace('@', '')        
-    elif len(cmds) == 1:
-        cmd = cmds[0]
+    userSender = getUserSender(msg)
 
-    if not cmd.startswith('/'):
+    ignoreTimeout = False
+    chat_id = msg['chat']['id']
+    user_id = msg["from"]['id']
+
+    if chat_id in ticketToHell and ticketToHell[chat_id] != user_id:
+        reply(msg, "a ti no te pregunte, metiche!")
+        return
+    elif chat_id in ticketToHell and ticketToHell[chat_id] == user_id and 'text' in msg:
+        ignoreTimeout = True
+        cmd = "/hell"
+        user = msg['text'].split(' ')[0].replace('@', '')
+        del ticketToHell[chat_id]
+    elif 'text' in msg:
+        cmds = msg['text'].split(' ')
+        response = ''
+        if len(cmds) >= 2:
+            cmd = cmds[0]
+            user = cmds[1].replace('@', '')        
+        elif len(cmds) == 1:
+            cmd = cmds[0]
+    elif 'sticker' in msg and msg['sticker']['file_id']==ticket:
+        if not validTimeout(msg, userSender):
+            return        
+        waitToHell(msg)
         return
 
-    if user == 'HELLORHEAVENBOT':
+    if not cmd.startswith('/') and not 'sticker' in msg:
+        return
+
+    if not ignoreTimeout and not validTimeout(msg, userSender):
+        return
+
+    if user.upper() == 'HELLORHEAVENBOT':
         reply(msg, 'si tu, voy corriendo!')
         return 
 
-    userSender = getUserSender(msg)
-    if user == userSender.upper():
+    if user.upper() == userSender.upper():
         reply(msg, u'solo dios puede juzgarte... nah!, los demas lo haran \U0001f602')
         return
     cmd = cmd.replace('@HellOrHeavenBot','')
-    if cmd == "/hell":
+    
+    if cmd == '':
+        return
+    elif cmd == "/hell":
         if user == '':
             reply(msg, 'que raro que tu... lee el manual!')
-            return    
+            return 
 
-        if user in stats:
-            stats[user]['hell'] += 1
-        else:
-            stats[user] = {'hell': 1, 'heaven': 0}
-        
-        response = getAwnser('hell')
+        dao.Update(user,dao.HELL)
+        response = getAwnser(dao.HELL)
+        newRecord(userSender)
     elif cmd == "/heaven":
         if user == '':
             reply(msg, 'que raro que tu... lee el manual!')
             return
 
-        if user in stats:
-            stats[user]['heaven'] += 1
-        else:
-            stats[user] = {'hell': 0, 'heaven': 1}
-            
-        response = getAwnser('heaven')
+        dao.Update(user, dao.HEAVEN)
+        response = getAwnser(dao.HEAVEN)
+        
+        newRecord(userSender)
     elif cmd == "/stats":
-        userKey = userSender.upper()
+        stats = dao.GetStats(userSender)
 
-        if userKey in stats:
-            hell = stats[userKey]['hell']
-            heaven = stats[userKey]['heaven']
+        if stats != []:
+            hell = stats[0]['hell']
+            heaven = stats[0]['heaven']
             emoji = u'\U0001f608'
             
             if heaven > hell:
@@ -143,10 +179,12 @@ def on_chat_message(msg):
             reply(msg, "solo dios tiene ese poder")
             return
         
+        stats = dao.GetAllStats()
+
         if len(stats) == 0:
             return
 
-        for key, val in stats.items():
+        for val in stats:
             heaven = val['heaven']
             hell = val["hell"]
             
@@ -155,10 +193,11 @@ def on_chat_message(msg):
             if heaven > hell:
                 emoji = u'\u271d\ufe0f'
 
-            response += '- {} -> Heaven: {}, Hell: {} ... {}\n'.format(key,  heaven, hell, emoji)
+            response += '- {} -> Heaven: {}, Hell: {} ... {}\n'.format(val["user"],  heaven, hell, emoji)
     else:
-        response = 'por no saber leer manuales te iras al infierno'
-    
+        return
+        # response = 'por no saber leer manuales te iras al infierno'
+        
     reply(msg, response)
 
 bot.message_loop({'chat': on_chat_message})
@@ -166,4 +205,4 @@ bot.message_loop({'chat': on_chat_message})
 print('Listening ...')
 
 while 1:
-    time.sleep(10)
+    time.sleep(10000)
