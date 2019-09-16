b852d31ce9193775f95fbce6454d985baf9e2dbd
Rey David <rdominguez@tecnologer.net>
2019-09-16 00:57:30 -0700

    add function to get sender name and function to create a hash from a srting

--- resources/utilities.go
+++ resources/utilities.go
@@ -1,9 +1,13 @@
 package resources
 
 import (
+	"fmt"
+	"hash/fnv"
 	"math/rand"
 	"regexp"
 	"time"
+
+	bot "github.com/yanzay/tbot"
 )
 
 var rgAtSignPrefix *regexp.Regexp
@@ -21,4 +25,24 @@ func LeftTrimAtSign(text string) string {
 func GetRandomIntFromRange(min, max int) int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(max-min) + min
+}
+
+//GetName extrats the username of the user who send the message
+func GetName(msg *bot.Message) string {
+	if msg.From.Username != "" {
+		return msg.From.Username
+	}
+
+	return fmt.Sprintf("%s %s", msg.From.FirstName, msg.From.LastName)
+}
+
+//GetHash create a hash value from values
+func GetHash(elements ...interface{}) string {
+	return hash(fmt.Sprint(elements...))
+}
+
+func hash(s string) string {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return string(h.Sum32())
 }
