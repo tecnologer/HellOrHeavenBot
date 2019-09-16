a36fc9a6bb8dc1c828c14bef5fbe657eb0402dc7
Rey David <rdominguez@tecnologer.net>
2019-09-16 01:59:29 -0700

    messenger: add function to send gifs

--- core/messenger.go
+++ core/messenger.go
@@ -14,20 +14,29 @@ func SendResponse(msg *bot.Message, res *m.Response) {
 		sendText(msg, res.Content)
 	case m.Sticker:
 		sendSticker(msg, res.Content)
+	case m.Gif:
+		sendGif(msg, res.Content)
 	}
 }
 
 func sendText(msg *bot.Message, text string) {
 	text = emoji.Sprint(text)
-	_, err := Client.SendMessage(msg.Chat.ID, text)
+	_, err := Client.SendMessage(msg.Chat.ID, text, bot.OptParseModeMarkdown)
 	if err != nil {
-		log.Println(err)
+		log.WithError(err).WithField("text", text).Error("error when try send text")
 	}
 }
 
 func sendSticker(msg *bot.Message, stickerID string) {
 	_, err := Client.SendSticker(msg.Chat.ID, stickerID)
 	if err != nil {
-		log.Println(err)
+		log.WithError(err).WithField("stickerID", stickerID).Error("error when try send sticker")
+	}
+}
+
+func sendGif(msg *bot.Message, gifID string) {
+	_, err := Client.SendDocument(msg.Chat.ID, gifID)
+	if err != nil {
+		log.WithError(err).WithField("gifId", gifID).Error("error when try send gif")
 	}
 }
