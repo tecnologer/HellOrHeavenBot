d69e4e8bf260795bb6c40f36c87ecb22a003bb96
Rey David <rdominguez@tecnologer.net>
2019-09-16 15:33:23 -0700

    strings: add message genericCancel

--- lang/strings.go
+++ lang/strings.go
@@ -7,11 +7,13 @@ var messages = languageList{
 		"genericFail":            "Something was wrong!",
 		"requestResponseContent": "The next message you sent will be taken as response. You can use text, sticker or gif",
 		"responseStored":         "Response stored correctly",
+		"genericCancel":          "Action canceled",
 	},
 	"es": map[string]string{
 		"ticketsNameRequired":    "El nombre del condenado es requerido",
 		"genericFail":            "falio ferga!",
 		"requestResponseContent": "El siguiente mensaje que envies se tomara como respuesta. Puedes user texto, sticker o gif",
 		"responseStored":         "Listoooo!!",
+		"genericCancel":          "Accion cancelada",
 	},
 }
