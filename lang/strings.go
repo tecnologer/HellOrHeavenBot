7d117f8f2011603d8a81d427e4399aa0875e76cc
Rey David <rdominguez@tecnologer.net>
2019-09-16 00:57:57 -0700

    add message (en/es) for "requestResponseContent"

--- lang/strings.go
+++ lang/strings.go
@@ -3,11 +3,13 @@ package lang
 //messages has the strings for static messages, groupped by language
 var messages = languageList{
 	"en": map[string]string{
-		"ticketsNameRequired": "The user name is required",
-		"genericFail":         "Something was wrong!",
+		"ticketsNameRequired":    "The user name is required",
+		"genericFail":            "Something was wrong!",
+		"requestResponseContent": "The next message you sent will be taken as response. You can use text, sticker or gif",
 	},
 	"es": map[string]string{
-		"ticketsNameRequired": "El nombre del condenado es requerido",
-		"genericFail":         "falio ferga!",
+		"ticketsNameRequired":    "El nombre del condenado es requerido",
+		"genericFail":            "falio ferga!",
+		"requestResponseContent": "El siguiente mensaje que envies se tomara como respuesta. Puedes user texto, sticker o gif",
 	},
 }
