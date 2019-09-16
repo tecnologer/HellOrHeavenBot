9da9690b756f13d6fb49927aa0733702731d5879
Rey David <rdominguez@tecnologer.net>
2019-09-16 15:33:45 -0700

    cancel action: created core functions

---
+++ core/cancelaction.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/tecnologer/HellOrHeavenBot/db"
+	bot "github.com/yanzay/tbot"
+)
+
+//Cancel cancels the current task
+func Cancel(msg *bot.Message) {
+	if HasUserIncompleteRes(msg) {
+		identifier := getResponseIdentifier((msg))
+		delete(incompleteResponse, identifier)
+	}
+
+	res, err := db.GetResponseByCommand(AcceptedCommands.GetID("cancel"), msg.From.LanguageCode)
+	if err != nil {
+		log.WithError(err).Info("error when try get response for cancel command")
+		sendText(msg, cLang["genericCancel"])
+	} else {
+		SendResponse(msg, res)
+	}
+}
