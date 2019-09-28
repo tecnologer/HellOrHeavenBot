2900e9fd60c95060e8af2fd0f36fa1993af55444
Rey David <rdominguez@tecnologer.net>
2019-09-28 16:43:34 -0700

    create group of conditions

--- db/dbhelp/condition.go
+++ db/dbhelp/condition.go
@@ -2,6 +2,13 @@ package dbhelp
 
 import "fmt"
 
+//ConditionGroup is group of SQL Condition, joined with logic operator
+type ConditionGroup struct {
+	ConLeft  *Condition
+	LogOp    SQLLogOperator
+	ConRight *Condition
+}
+
 //Condition is a SQL Condition
 type Condition struct {
 	Column *SQLColumn
@@ -10,7 +17,7 @@ type Condition struct {
 }
 
 //ToString parse to SQL transact the condition
-func (c Condition) ToString() string {
+func (c *Condition) ToString() string {
 	pattern := "[%s] %s %v"
 	if c.Column.Type == SQLTypeText {
 		pattern = "[%s] %s '%v'"
@@ -22,4 +29,13 @@ func (c Condition) ToString() string {
 	}
 
 	return fmt.Sprintf(pattern, c.Column.Name, c.RelOp, c.Value)
+}
+
+//ToString parse to SQL transact the group of conditions
+func (g ConditionGroup) ToString() string {
+	if g.ConRight == nil {
+		return fmt.Sprintf("%s", g.ConLeft.ToString())
+	}
+
+	return fmt.Sprintf("%s %s %s", g.ConLeft.ToString(), g.LogOp, g.ConRight.ToString())
 }
