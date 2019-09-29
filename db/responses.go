876d1fe874e8c060e012fda1ea6f1db686f1d3b1
Rey David <rdominguez@tecnologer.net>
2019-09-29 15:55:54 -0700

    implement SQLTable

--- db/responses.go
+++ db/responses.go
@@ -1,91 +1,92 @@
 package db
 
 import (
-	log "github.com/sirupsen/logrus"
+	"fmt"
+
+	hpr "github.com/tecnologer/HellOrHeavenBot/db/dbhelp"
 	"github.com/tecnologer/HellOrHeavenBot/model"
+	"github.com/tecnologer/HellOrHeavenBot/resources"
 )
 
-var tableResponsesIsCreated bool
-
-const (
-	tableNameResponses        = "Response"
-	queryCreateTableResponses = `CREATE TABLE IF NOT EXISTS [%s] (
-		[Id] integer not null primary key AUTOINCREMENT,
-		[Type] integer not null,
-		[CommandID] integer not null,
-		[Response] text not null,
-		[Language] text not null,
-		[IsAnimated] integr
-	);`
-	queryGetResponseCommand = "SELECT [CommandID], [Response],[Type], [IsAnimated] FROM [%s] WHERE [CommandID] = %d AND [Language] = '%s';"
-	queryInsertResponse     = "INSERT INTO [%s] (Type, CommandID, Response, Language, IsAnimated) VALUES (%d, %d, '%s', '%s', %t);"
-)
-
-func init() {
-	createTableResponses()
-}
-
-func createTableResponses() {
-	log.Debugf("creating table %s\n", tableNameResponses)
-	if tableResponsesIsCreated {
-		return
-	}
-
-	// err := execQueryNoResult(queryf(queryCreateTableResponses, tableNameResponses))
-	// tableResponsesIsCreated = err == nil
-	// if !tableResponsesIsCreated {
-	// 	log.WithError(err).Errorf("error when try create table %s", tableNameResponses)
-	// } else {
-	// 	log.Debugf("table %s is created\n", tableNameResponses)
-	// }
-
+var responsesTable = &hpr.SQLTable{
+	Name: "ChatLog",
+	Columns: []*hpr.SQLColumn{
+		hpr.NewPKCol("Id"),
+		hpr.NewIntCol("Type"),
+		hpr.NewIntCol("CommandID"),
+		hpr.NewTextCol("Response"),
+		hpr.NewTextCol("Language"),
+		hpr.NewIntNilCol("IsAnimated"),
+	},
 }
 
 //InsertResponse creates new record of response in Response
 func InsertResponse(res *model.Response) error {
-	createTableResponses()
+	err := responsesTable.Create()
+	if err != nil {
+		return err
+	}
 
-	// tmpQuery := queryf(queryInsertResponse, tableNameResponses, res.Type, res.CommandID, res.Content, res.Language, res.IsAnimated)
-
-	// err := execQueryNoResult(tmpQuery)
-	// if err != nil {
-	// 	return err
-	// }
+	err = responsesTable.Insert(res.Type, res.CommandID, res.Content, res.Language, res.IsAnimated)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 //GetResponseByCommand select a response (random) assigned to the command
 func GetResponseByCommand(comID int, lang string) (*model.Response, error) {
-	// tmpQuery := queryf(queryGetResponseCommand, tableNameResponses, comID, lang)
-	// rows, err := execQuery(tmpQuery)
+	err := responsesTable.Create()
+	if err != nil {
+		return nil, err
+	}
 
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// defer rows.Close()
-	// var responses []*model.Response
-	// for rows.Next() {
-	// 	response := new(m.Response)
+	conditions := []*hpr.ConditionGroup{
+		&hpr.ConditionGroup{
+			ConLeft: &hpr.Condition{
+				Column: chatsTable.GetColByName("CommandID"),
+				RelOp:  hpr.Eq,
+				Value:  comID,
+			},
+			LogOp: hpr.And,
+			ConRight: &hpr.Condition{
+				Column: chatsTable.GetColByName("Language"),
+				RelOp:  hpr.Eq,
+				Value:  lang,
+			},
+		},
+	}
 
-	// 	err = rows.Scan(&response.CommandID, &response.Content, &response.Type)
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	responses = append(responses, response)
-	// }
-	// err = rows.Err()
-	// if err != nil {
-	// 	return nil, err
-	// }
+	rows, err := responsesTable.ExecSelectCols([]string{}, conditions)
 
-	// if len(responses) == 0 {
-	// 	return nil, fmt.Errorf("no responses for the selected command")
-	// } else if len(responses) == 1 {
-	// 	return responses[0], nil
-	// }
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var responses []*model.Response
+	for rows.Next() {
+		response := new(model.Response)
 
-	// selection := resources.GetRandomIntFromRange(0, len(responses)-1)
+		err = rows.Scan(&response.CommandID, &response.Content, &response.Type)
+		if err != nil {
+			return nil, err
+		}
+		responses = append(responses, response)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
 
-	// return responses[selection], nil
-	return nil, nil
+	if len(responses) == 0 {
+		return nil, fmt.Errorf("no responses for the selected command")
+	}
+
+	if len(responses) == 1 {
+		return responses[0], nil
+	}
+
+	selection := resources.GetRandomIntFromRange(0, len(responses)-1)
+
+	return responses[selection], nil
 }
