e96c4777eb80aaf7e247c6268947bc51a1bc88de
Rey David <rdominguez@tecnologer.net>
2019-09-29 21:52:47 -0700

    Custom Response: create function to retrieve and insert custom response

--- core/customResponse.go
+++ core/customResponse.go
@@ -1,6 +1,85 @@
 package core
 
-//hasCustomResponse checks if the text match with some regex
-func hasCustomResponse(text string) {
+import (
+	"regexp"
 
+	log "github.com/sirupsen/logrus"
+	"github.com/tecnologer/HellOrHeavenBot/db"
+	"github.com/tecnologer/HellOrHeavenBot/model"
+	"github.com/tecnologer/HellOrHeavenBot/resources"
+	bot "github.com/yanzay/tbot"
+)
+
+//hasCustomResponse checks if the text match with some regex
+func hasCustomResponse(msg *bot.Message) {
+	if msg.Text == "" {
+		return
+	}
+
+	readyResponse := make([]*model.Response, 0)
+	defer func() {
+		if r := recover(); r != nil {
+			log.WithFields(log.Fields{
+				"Responses": readyResponse,
+				"text":      msg.Text,
+				"chatID":    msg.Chat.ID,
+			}).Debug(r)
+		}
+	}()
+
+	responses, err := db.RetrieveCustomResponses(msg.Chat.ID)
+
+	if err != nil {
+		log.WithError(err).WithFields(log.Fields{
+			"text":   msg.Text,
+			"chatID": msg.Chat.ID,
+		}).Error("error when try retrive custom responses")
+		return
+	}
+
+	if len(responses) == 0 {
+		return
+	}
+
+	for _, res := range responses {
+		regex, err := regexp.Compile(res.Regex)
+		if err != nil {
+			log.WithError(err).WithFields(log.Fields{
+				"text":   msg.Text,
+				"chatID": msg.Chat.ID,
+				"regex":  res.Regex,
+			}).Error("error when try compile the regex")
+			continue
+		}
+
+		if regex.Match([]byte(msg.Text)) {
+			readyResponse = append(readyResponse, &model.Response{Content: res.Response, Type: res.ResponseType})
+		}
+	}
+
+	if len(readyResponse) == 0 {
+		return
+	}
+
+	if len(readyResponse) == 1 {
+		SendResponse(msg, readyResponse[0])
+		return
+	}
+
+	selection := resources.GetRandomIntFromRange(0, len(readyResponse)-1)
+
+	SendResponse(msg, readyResponse[selection])
+}
+
+func AddCustomResponse(cRes *model.CustomResponse) error {
+	_, err := regexp.Compile(cRes.Regex)
+
+	if err != nil {
+		log.WithError(err).WithFields(log.Fields{
+			"CustomResponse": cRes,
+		}).Error("error when try compile the regex")
+		return err
+	}
+
+	return db.InsertCustomResponse(cRes)
 }
--- db/customResponse.go
+++ db/customResponse.go
@@ -15,22 +15,69 @@ var customResTable = &hpr.SQLTable{
 		hpr.NewTextCol("Regex"),
 		hpr.NewTextCol("Response"),
 		hpr.NewIntCol("ResponseType"),
-		hpr.NewIntNilCol("ChatID"),
+		hpr.NewTextNilCol("ChatID"),
 		hpr.NewIntNilCol("Author"),
 	},
 }
 
-func init() {
-	customResTable.Create()
+//RetrieveCustomResponses returns an array with the CustomReponse for the selected chat
+func RetrieveCustomResponses(chatID string) ([]*model.CustomResponse, error) {
+	err := customResTable.Create()
+	if err != nil {
+		return nil, err
+	}
+
+	conditions := []*hpr.ConditionGroup{
+		&hpr.ConditionGroup{
+			ConLeft: &hpr.Condition{
+				Column: customResTable.GetColByName("ChatID"),
+				RelOp:  hpr.Eq,
+				Value:  chatID,
+			},
+			LogOp: hpr.Or,
+			ConRight: &hpr.Condition{
+				Column: customResTable.GetColByName("ChatID"),
+				RelOp:  hpr.Eq,
+				Value:  nil,
+			},
+		},
+	}
+
+	rows, err := customResTable.ExecSelectCols([]string{"Regex", "Response", "ResponseType"}, conditions)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	responses := make([]*model.CustomResponse, 0)
+	for rows.Next() {
+		res := new(model.CustomResponse)
+
+		err = rows.Scan(&res.Regex, &res.Response, &res.ResponseType)
+		if err != nil {
+			return nil, err
+		}
+
+		responses = append(responses, res)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return responses, nil
 }
 
-const (
-	queryGetCResponseCommand = "SELECT [CommandID], [Response],[Type] FROM [%s] WHERE [CommandID] = %d AND [Language] = '%s';"
-	queryCInsertResponse     = "INSERT INTO [%s] (Type, CommandID, Response, Language) VALUES (%d, %d, '%s', '%s', %t);"
-)
+func InsertCustomResponse(cRes *model.CustomResponse) error {
+	err := customResTable.Create()
+	if err != nil {
+		return err
+	}
 
-//RetrieveCustomResponses returns an array with the CustomReponse for the selected chat
-func RetrieveCustomResponses(chatID int) ([]*model.CustomResponse, error) {
+	var chatID interface{}
 
-	return nil, nil
+	if cRes.ChatID != "" {
+		chatID = cRes.ChatID
+	}
+	return customResTable.Insert(cRes.Regex, cRes.Response, cRes.ResponseType, chatID, cRes.Author)
 }
